c79ce61891add1aed419ab315d564b6c82abf5f9
eemi2010 <jazzmatadazz@gmail.com>
2014-01-22 22:33:33 +0100

    brand new ext4j site inspired from the errai page

---
+++ js/common.js
@@ -0,0 +1,12 @@
+
+  // slider
+  $('#da-slider').cslider({
+	autoplay    : true,
+	interval    : 10000,
+  initialInterval: 3000
+  });
+
+  // lightbox gallery
+  $(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true});
+
+  // social sharing
