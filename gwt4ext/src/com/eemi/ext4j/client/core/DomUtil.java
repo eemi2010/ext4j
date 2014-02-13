f59f54ca887ed2b167b6f365fe264743e133b242
eemi2010 <jazzmatadazz@gmail.com>
2014-02-14 00:06:03 +0100

    removed deprecated calls to DOM class

--- gwt4ext/src/com/eemi/ext4j/client/core/DomUtil.java
+++ gwt4ext/src/com/eemi/ext4j/client/core/DomUtil.java
@@ -1,24 +1,22 @@
 /**
- Ext4j UI Library
- Copyright 2014, Alain Ekambi, and individual contributors as indicated
- by the @authors tag. See the copyright.txt in the distribution for a
- full listing of individual contributors.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Ext4j UI Library Copyright 2014, Alain Ekambi, and individual contributors as
+ * indicated by the @authors tag. See the copyright.txt in the distribution for
+ * a full listing of individual contributors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.eemi.ext4j.client.core;
 
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -33,7 +31,7 @@ public class DomUtil {
     }
 
     public static String getID(Element element) {
-        String id = DOM.getElementProperty(element, "id");
+        String id = element.getPropertyString("id");
         return id == null || id.equals("") ? null : id;
 
     }
@@ -43,7 +41,7 @@ public class DomUtil {
     }
 
     public static void setID(Element element, String id) {
-        DOM.setElementProperty(element, "id", id);
+        element.setPropertyString("id", id);
     }
 
 }
